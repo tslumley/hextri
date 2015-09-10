187bff4e28106f21c07c3b5342ddfec831af748d
Thomas Lumley <t.lumley@auckland.ac.nz>
2015-09-10 16:12:23 +1000

    Error diffusion for rare classes

--- R/hextri.R
+++ R/hextri.R
@@ -18,17 +18,28 @@ sainte_lague= function(votes, nseats){
    clear<-rowSums(quotients>last)
    borderline<-rowSums(quotients==last)
    borderline[sample(which(borderline>0),sum(borderline)-(nseats-sum(clear)))]<-0
-   rep(1:nparties,clear+borderline)
+   total<-clear+borderline
+   error<- votes- sum(votes)*total/6
+   rval<-rep(1:nparties,clear+borderline)
+   attr(rval,"error")<-error
+   rval
 } 
 
 
-hextri<-function(x,y,class,colours,nbins=10,border=TRUE, 
+hextri<-function(x,y,class,colours,nbins=10,border=TRUE, diffuse=FALSE,
         style=c("alpha","size")){
   style<-match.arg(style)
+  if(!diffuse){
   switch(style,
     size=hexclass(x,y,class,colours,nbins=nbins,border=border),
     alpha=hexclass1(x,y,class,colours,nbins=nbins,border=border)
   )
+  } else {
+  	switch(style,
+    size=hexclass_diffuse(x,y,class,colours,nbins=nbins,border=border),
+    alpha=hexclass1_diffuse(x,y,class,colours,nbins=nbins,border=border)
+  )
+  }
 }
 
 hexclass<-function(x,y,class,colours,nbins=10,border=FALSE){
@@ -60,18 +71,31 @@ hexclass1<-function(x,y,class,colours,nbins=10,border=FALSE){
 }
 
 
-hexclass1_rand<-function(x,y,class,colours,nbins=10,border=FALSE){
+hexclass_diffuse<-function(x,y,class,colours,nbins=10,border=FALSE){
 	plot(x,y,type="n")
 	h<-hexbin(x,y,IDs=TRUE,xbins=nbins)
 	centers<-hcell2xy(h)
 	tab<-table(h@cID,class)
-	allocations<-apply(tab,1,sainte_lague,6)
+	allocations<-diffuse(h,tab)
+	full_radius<-diff(h@xbnds)/h@xbins/sqrt(3)
+	radii<-full_radius*sqrt(h@count/max(h@count))
+	col_matrix<-matrix(colours[t(allocations)],nrow=ncol(allocations))
+	hexen(centers$x,centers$y,radii, col_matrix,border=border)
+}
+
+
+hexclass1_diffuse<-function(x,y,class,colours,nbins=10,border=FALSE){
+	plot(x,y,type="n")
+	h<-hexbin(x,y,IDs=TRUE,xbins=nbins)
+	centers<-hcell2xy(h)
+	tab<-table(h@cID,class)
+	allocations<-diffuse(h,tab)
 	full_radius<-diff(h@xbnds)/h@xbins/sqrt(3)
 	alpha<-h@count/max(h@count)
 	all_colours<-colours[t(allocations)]
 	rgb<-col2rgb(all_colours)
 	alpha_colours<-rgb(rgb[1,],rgb[2,],rgb[3,],255*alpha,max=255)
 	col_matrix<-matrix(alpha_colours,nrow=ncol(allocations))
-	for(i in 1:nrow(col_matrix)) col_matrix[i,]<-sample(col_matrix[i,])
 	hexen(centers$x,centers$y,rep(full_radius,length(centers$x)), col_matrix,border=border)
 }
+
